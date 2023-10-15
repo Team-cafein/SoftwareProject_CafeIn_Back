27cc9ec2f265e8116f358642a1954c1a1516ab6c
kscold <chan6502@gmail.com>
2023-10-15 23:18:35 +0900

    GPT 백엔드 업데이트

--- src/api/gpt/gpt.js
+++ src/api/gpt/gpt.js
@@ -3,31 +3,50 @@ import axios from 'axios';
 require('dotenv').config();
 const { OPENAI_SECRET_KEY } = process.env;
 
-const openaiSecretKey = process.env.OPENAI_SECRET_KEY;
-
-if (openaiSecretKey) {
-  console.log('OPENAI_SECRET_KEY is defined:', openaiSecretKey);
-} else {
-  console.log('OPENAI_SECRET_KEY is not defined.');
-}
-
 const gptAPI = axios.create({
-  baseURL: 'https://api.openai.com/v1/engines/davinci/completions',
+  baseURL: 'https://api.openai.com',
   headers: {
     Authorization: `Bearer ${OPENAI_SECRET_KEY}`,
     'Content-Type': 'application/json',
   },
 });
 
-export const askGpt = async (prompt) => {
+export const askGpt = async (message) => {
   try {
-    const response = await gptAPI.post('', {
-      prompt,
-      max_tokens: 50,
+    const response = await gptAPI.post('/v1/chat/completions', {
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: message }],
     });
-    return response.data.choices[0].text;
+
+    const repliedMessage = response.data.choices[0].message.content;
+
+    return repliedMessage;
   } catch (error) {
     console.error(error);
-    throw error;
   }
 };
+
+// src/api/gpt/gpt.js
+// const { Configuration, OpenAIApi } = require('openai');
+
+// import { Configuration, OpenAIApi } from 'openai';
+
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_SECRET_KEY,
+// });
+
+// const openai = new OpenAIApi(configuration);
+
+// export const askGpt = async (prompt) => {
+//   try {
+//     const response = await openai.createCompletion({
+//       model: 'gpt-3.5-turbo',
+//       prompt: prompt,
+//       max_tokens: 50,
+//     });
+//     return response.data.choices[0].text;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
--- src/api/index.js
+++ src/api/index.js
@@ -14,5 +14,41 @@ api.use('/cafe', cafe.routes()); // cafe 모듈의 라우터를 사용합니다.
 api.use('/reviews', reviews.routes());
 api.use('/gpt', gpt.routes()); // GPT 라우터 추가
 
+// const axios = require('axios');
+// // dotenv는 .env 파일을 읽어서 process.env에 설정해 줍니다.
+// const dotenv = require('dotenv');
+
+// dotenv.config();
+
+// const openaiSecretKey = process.env.OPENAI_SECRET_KEY;
+
+// if (openaiSecretKey) {
+//   console.log('OPENAI_SECRET_KEY is defined:', openaiSecretKey);
+// } else {
+//   console.log('OPENAI_SECRET_KEY is not defined.');
+// }
+
+// axios
+//   .post(
+//     'https://api.openai.com/v1/chat/completions',
+//     {
+//       model: 'gpt-3.5-turbo',
+//       messages: [{ role: 'user', content: '안녕 AI' }],
+//     },
+//     {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${openaiSecretKey}`,
+//       },
+//     },
+//   )
+//   .then((response) => {
+//     console.log(response.data);
+//     console.log(response.data.choices[0].message.content);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
 // 라우터를 내보냄
 export default api;
