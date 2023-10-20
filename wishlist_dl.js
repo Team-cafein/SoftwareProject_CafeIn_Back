513f17b2b9785a549d6ad45232896c2dba708f46
311516h <82697724+311516h@users.noreply.github.com>
2023-10-20 18:56:54 +0900

    찜하기 기능 update

--- wishlist_dl.js
+++ wishlist_dl.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// MongoDB 연결 설정
-mongoose.connect('mongodb://localhost/product-wishlist', { useNewUrlParser: true, useUnifiedTopology: true });
+// // MongoDB 연결 설정
+// mongoose.connect('mongodb://localhost/product-wishlist', { useNewUrlParser: true, useUnifiedTopology: true });
 
-const ProductWishlist = mongoose.model('ProductWishlist', {
-  userId: String,
-  productId: String,
-});
+// const ProductWishlist = mongoose.model('ProductWishlist', {
+//   userId: String,
+//   productId: String,
+// });
 
-// 삭제할 데이터 조건 설정
-const condition = { userId: "user123" };
+// // 삭제할 데이터 조건 설정
+// const condition = { userId: "user123" };
 
-// 데이터 삭제 (Promise 사용)
-ProductWishlist.deleteMany(condition)
-  .then(() => {
-    console.log('데이터 삭제 완료');
-  })
-  .catch((err) => {
-    console.error('데이터 삭제 실패:', err);
-  });
+// // 데이터 삭제 (Promise 사용)
+// ProductWishlist.deleteMany(condition)
+//   .then(() => {
+//     console.log('데이터 삭제 완료');
+//   })
+//   .catch((err) => {
+//     console.error('데이터 삭제 실패:', err);
+//   });
