1925556ce00784cd5287d154a1a05665d6877fa8
kscold <chan6502@gmail.com>
2023-12-10 22:24:18 +0900

    취향 재조사 업데이트

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -13,7 +13,7 @@ auth.get('/check', authCtrl.check);
 auth.post('/logout', authCtrl.logout);
 
 auth.post('/register/question', checkUserExists, questionsCtrl.createQuestion); // 처음 질문과 답변을 저장하는 엔드포인트 추가
-auth.put('/register/question', checkLoggedIn, questionsCtrl.updateQuestion); // 질문을 업데이트하는 새로운 엔드포인트 추가
+auth.put('/register/question', checkUserExists, questionsCtrl.updateQuestion); // 질문을 업데이트하는 새로운 엔드포인트 추가
 auth.get('/register/answer', checkLoggedIn, questionsCtrl.getProfile);
 
 export default auth;
